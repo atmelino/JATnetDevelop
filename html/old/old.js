76ad6ccf85d248967bac8c4f0e3b0f1104eea219
jatnik <jatnik@jat>
2015-01-16 18:05:33 -0600

    background selection

--- html/old/old.js
+++ html/old/old.js
@@ -1,3 +1,12 @@
+		// skyBox/FOG
+		var skyBoxGeometry = new THREE.CubeGeometry(1000 * gridSize, 1000 * gridSize, 1000 * gridSize);
+		var skyBoxMaterial = new THREE.MeshBasicMaterial({
+			color : 0x9999ff,
+			side : THREE.BackSide
+		});
+		var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
+		scene.add(skyBox);
+		//scene.fog = new THREE.FogExp2(0x9999ff, 0.00025);
 
 	
 	var BGChanger = function() {
